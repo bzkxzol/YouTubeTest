5f53a9aa1370d6efa439c13d4ea839223dc76ce0
bzkxzol <30575362+bzkxzol@users.noreply.github.com>
2017-09-15 21:14:01 +0300

    Local driver

--- src/test/java/Tests/Tests.java
+++ src/test/java/Tests/Tests.java
@@ -10,17 +10,23 @@ public class Tests extends ChromeSettings {
     @Test
     public void searchVideo() throws InterruptedException {
         SearchPage searchPage = new SearchPage(driver);
+        System.out.println("Step 1");
         VideoPage videoPage = new VideoPage(driver);
+        System.out.println("Step 2");
         searchPage.typeVideoName();
+        System.out.println("Step 3");
         searchPage.videoClick();
-
+        System.out.println("Step 4");
         String winHandleBefore = driver.getWindowHandle();
+        System.out.println("Step 5");
         videoPage.muteVideo();
         Thread.sleep(15000);
+        System.out.println("Step 6");
         videoPage.clickOnAdvert();
+        System.out.println("Step 7");
         driver.switchTo().window(winHandleBefore);
         Thread.sleep(1000);
-
+        System.out.println("Step 8");
         videoPage.playVideoClick();
     }
 }
